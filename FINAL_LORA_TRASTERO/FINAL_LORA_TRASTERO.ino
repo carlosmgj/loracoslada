015d38688e1411cd5980de8b6364fd3e89e46148
Carlos <100377526@alumnos.uc3m.es>
2024-03-03 13:05:30 +0100

    Primera versión funcionando de trastero

--- FINAL_LORA_TRASTERO/FINAL_LORA_TRASTERO.ino
+++ FINAL_LORA_TRASTERO/FINAL_LORA_TRASTERO.ino
@@ -10,9 +10,9 @@
 #include <RH_RF95.h>
 
 // Feather 32u4:
-#define RFM95_CS   8
-#define RFM95_RST  4
-#define RFM95_INT  7
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 7
 
 #define CLIENT_ADDRESS 1
 #define SERVER_ADDRESS 2
@@ -23,10 +23,29 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+unsigned long startMillis;
+unsigned long currentMillis;
+const unsigned long period = 10000;  //ms
+
+bool estado;
+unsigned long startMillis2;
+unsigned long currentMillis2;
+const unsigned long period2 = 1000;  //ms
+
+const byte interruptPin = 11;
+int pushButton = 11;
+
+const char *messageToSend = "HeartBeat";  // Cambia este valor según tu necesidad
+uint8_t len = strlen(messageToSend) + 1;  // Incluye el carácter nulo '\0'
+
+
 void setup() {
   pinMode(RFM95_RST, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  estado = digitalRead(pushButton);
 
   Serial.begin(115200);
   //while (!Serial) delay(1);
@@ -43,16 +62,19 @@ void setup() {
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
-    while (1);
+    while (1)
+      ;
   }
   Serial.println("LoRa radio init OK!");
 
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
-    while (1);
+    while (1)
+      ;
   }
-  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  Serial.print("Set Freq to: ");
+  Serial.println(RF95_FREQ);
 
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
@@ -60,40 +82,97 @@ void setup() {
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
+
+  startMillis = millis();   //initial start time
+  startMillis2 = millis();  //initial start time
 }
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void loop() {
-  const char *messageToSend = "Puerta abierta";  // Cambia este valor según tu necesidad
-  uint8_t len = strlen(messageToSend) + 1;  // Incluye el carácter nulo '\0'
+  currentMillis = millis();
+  currentMillis2 = millis();
+  if (currentMillis - startMillis >= period) {
 
-  Serial.println("Sending message to server...");
+    int buttonState = digitalRead(pushButton);
 
-  rf95.send((uint8_t *)messageToSend, len);
-  
-  if (rf95.waitPacketSent()) {
-    Serial.println("Message sent successfully!");
-    Serial.println("Waiting for ACK...");
+    // Verifica si el botón está presionado (estado bajo)
+    if (buttonState == HIGH) {
+      // Cambia el mensaje a "ALERTA"
+      messageToSend = "ALERTA";
+      len = strlen(messageToSend) + 1;
+    } else {
+      // Si el botón no está presionado, vuelve al mensaje "HeartBeat"
+      messageToSend = "HeartBeat";
+      len = strlen(messageToSend) + 1;
+    }
 
-    if (rf95.waitAvailableTimeout(3000)) {
-      uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-      uint8_t len = sizeof(buf);
-      
-      if (rf95.recv(buf, &len) && len > 0 && buf[0] == 'A') {
-        Serial.println("ACK received!");
-        // Hacer parpadear el LED en el transmisor
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(500);  // Tiempo de encendido del LED (ajusta según sea necesario)
-        digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("Sending HeartBeart message to server...");
+
+    rf95.send((uint8_t *)messageToSend, len);
+
+    if (rf95.waitPacketSent()) {
+      Serial.println("Message sent successfully!");
+      Serial.println("Waiting for ACK...");
+
+      if (rf95.waitAvailableTimeout(3000)) {
+        uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+        uint8_t len = sizeof(buf);
+
+        if (rf95.recv(buf, &len) && len > 0 && buf[0] == 'A') {
+          Serial.println("ACK received!");
+          // Hacer parpadear el LED en el transmisor
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(500);  // Tiempo de encendido del LED (ajusta según sea necesario)
+          digitalWrite(LED_BUILTIN, LOW);
+        } else {
+          Serial.println("Invalid or no ACK received.");
+        }
       } else {
-        Serial.println("Invalid or no ACK received.");
+        Serial.println("No ACK received within timeout.");
       }
     } else {
-      Serial.println("No ACK received within timeout.");
+      Serial.println("Error sending message. Retrying...");
     }
-  } else {
-    Serial.println("Error sending message. Retrying...");
+    startMillis = millis();  //initial start time
   }
-  delay(3000);
+  if (currentMillis2 - startMillis2 >= period2) {
+    {
+      bool estadonuevo = digitalRead(pushButton);
+      if (estado != estadonuevo) {
+        Serial.println("BOTON CAMBIADO");
+        estado = digitalRead(pushButton);
+        const char *messageToSend = "CAMBIO";     // Cambia este valor según tu necesidad
+        uint8_t len = strlen(messageToSend) + 1;  // Incluye el carácter nulo '\0'
+
+        Serial.println("Sending message CAMBIO to server...");
+
+        rf95.send((uint8_t *)messageToSend, len);
+
+        if (rf95.waitPacketSent()) {
+          Serial.println("Message sent successfully!");
+          Serial.println("Waiting for ACK...");
+
+          if (rf95.waitAvailableTimeout(3000)) {
+            uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+            uint8_t len = sizeof(buf);
+
+            if (rf95.recv(buf, &len) && len > 0 && buf[0] == 'A') {
+              Serial.println("ACK received!");
+              // Hacer parpadear el LED en el transmisor
+              digitalWrite(LED_BUILTIN, HIGH);
+              delay(500);  // Tiempo de encendido del LED (ajusta según sea necesario)
+              digitalWrite(LED_BUILTIN, LOW);
+            } else {
+              Serial.println("Invalid or no ACK received.");
+            }
+          } else {
+            Serial.println("No ACK received within timeout.");
+          }
+        } else {
+          Serial.println("Error sending message. Retrying...");
+        }
+      }
+    }
+  }
 }
